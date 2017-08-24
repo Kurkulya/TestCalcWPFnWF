a4ca395772b08832b2bad611404378d881d2abc9
Kurkulya <kulinich_v@hotmail.com>
2017-08-24 23:21:27 +0300

    Added object model

---
+++ TestCalcWPFnWF/ObjectModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems;
+
+namespace TestCalcWPFnWF
+{
+    public class OM
+    {
+        string but1  = "butOne";
+        string but2 = "butTwo";
+        string but3 = "butThree";
+        string but4 = "butFour";
+        string but5 = "butFive";
+        string but6 = "butSix";
+        string but7 = "butSeven";
+        string but8 = "butEight";
+        string but9 = "butNine";
+        string but0 = "butZero";
+        string butPlus = "butPlus";
+        string butMinus = "butMinus";
+        string butMult = "butMult";
+        string butDiv = "butDiv";
+        string butEqual = "butCalculate";
+        string txtResult = "txtResult";
+
+        Window window;
+        public OM(Window window)
+        {
+            this.window = window;
+        }
+
+        public Button GetButton(string s)
+        {
+            Button flag = null;
+            if(s == nameof(but1))
+                flag = window.Get<Button>(SearchCriteria.ByAutomationId(but1));
+            else if(s == nameof(but2))
+                flag = window.Get<Button>(SearchCriteria.ByAutomationId(but2));
+            else if (s == nameof(but3))
+                flag = window.Get<Button>(SearchCriteria.ByAutomationId(but3));
+            else if (s == nameof(but4))
+                flag = window.Get<Button>(SearchCriteria.ByAutomationId(but4));
+            else if (s == nameof(but5))
+                flag = window.Get<Button>(SearchCriteria.ByAutomationId(but5));
+            else if (s == nameof(but6))
+                flag = window.Get<Button>(SearchCriteria.ByAutomationId(but6));
+            else if (s == nameof(but7))
+                flag = window.Get<Button>(SearchCriteria.ByAutomationId(but7));
+            else if (s == nameof(but8))
+                flag = window.Get<Button>(SearchCriteria.ByAutomationId(but8));
+            else if (s == nameof(but9))
+                flag = window.Get<Button>(SearchCriteria.ByAutomationId(but9));
+            else if (s == nameof(but0))
+                flag = window.Get<Button>(SearchCriteria.ByAutomationId(but0));
+            else if (s == nameof(butMinus))
+                flag = window.Get<Button>(SearchCriteria.ByAutomationId(butMinus));
+            else if (s == nameof(butPlus))
+                flag = window.Get<Button>(SearchCriteria.ByAutomationId(butPlus));
+            else if (s == nameof(butDiv))
+                flag = window.Get<Button>(SearchCriteria.ByAutomationId(butDiv));
+            else if (s == nameof(butEqual))
+                flag = window.Get<Button>(SearchCriteria.ByAutomationId(butEqual));
+            else if (s == nameof(butMult))
+                flag = window.Get<Button>(SearchCriteria.ByAutomationId(butMult));         
+            return flag;
+        }
+
+        public TextBox GetTextBox(string s)
+        {
+            TextBox flag = null;
+            if (s == nameof(txtResult))
+                flag = window.Get<TextBox>(SearchCriteria.ByAutomationId(txtResult));   
+            return flag;
+        }
+    }
+}
+
---
+++ TestCalcWPFnWF/TestWFCalc.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.Factory;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Actions;
+using TestStack.White.UIItems.Finders;
+using System.Windows.Automation;
+
+namespace TestCalcWPFnWF
+{
+    [TestClass]
+    public class TestWFCalc
+    {
+        Window window = null;
+        OM obj;
+        [TestInitialize]
+        public void StartUp()
+        {
+            string appPath = @"E:\C# 1708\Calcs\With Buttons\CalcWFwithButtons\CalcWFwithButtons\bin\Debug\CalcWFwithButtons";
+            Application application = Application.Launch(appPath);
+            window = application.GetWindow("Calc", InitializeOption.NoCache);
+            obj = new OM(window);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            window.Close();
+        }
+
+        [DataTestMethod]
+        [DataRow("but1")]
+        [DataRow("but2")]
+        [DataRow("but3")]
+        [DataRow("but4")]
+        [DataRow("but5")]
+        [DataRow("but6")]
+        [DataRow("but6")]
+        [DataRow("but8")]
+        [DataRow("but9")]
+        [DataRow("but0")]
+        [DataRow("butMinus")]
+        [DataRow("butPlus")]
+        [DataRow("butMult")]
+        [DataRow("butDiv")]
+        [DataRow("butEqual")]
+        public void TestWPFExistingElement(string elId)
+        {
+            Assert.AreEqual(true, obj.GetButton(elId).Visible);
+        }
+
+        [DataTestMethod]
+        [DataRow("but1", "1")]
+        [DataRow("but2", "2")]
+        [DataRow("but3", "3")]
+        [DataRow("but4", "4")]
+        [DataRow("but5", "5")]
+        [DataRow("but6", "6")]
+        [DataRow("but7", "7")]
+        [DataRow("but8", "8")]
+        [DataRow("but9", "9")]
+        [DataRow("but0", "0")]
+        public void TestWPFSimpleCheck(string elId, string res)
+        {
+            obj.GetButton(elId).Click();
+            string calc = obj.GetTextBox("txtResult").BulkText;
+            Assert.AreEqual(res, calc);
+        }
+
+        [DataTestMethod]
+        [DataRow(new string[] { "but1", "but2", "but3" }, "123")]
+        [DataRow(new string[] { "but4", "but5", "but6" }, "456")]
+        [DataRow(new string[] { "but7", "but8", "but9" }, "789")]
+        [DataRow(new string[] { "but3", "but0", "but6" }, "306")]
+        public void TestWPFComplexCheck(string[] arr, string res)
+        {
+            foreach (string str in arr)
+            {
+                obj.GetButton(str).Click();
+            }
+            string calc = obj.GetTextBox("txtResult").BulkText;
+            Assert.AreEqual(res, calc);
+        }
+
+        [DataTestMethod]
+        [DataRow("but1", "but2", "butPlus", "3")]
+        [DataRow("but3", "but4", "butMinus", "-1")]
+        [DataRow("but5", "but6", "butMult", "30")]
+        [DataRow("but9", "but3", "butDiv", "3")]
+        public void TestWPFRealJob(string x, string y, string op, string res)
+        {
+            obj.GetButton(x).Click();
+            obj.GetButton(op).Click();
+            obj.GetButton(y).Click();
+            obj.GetButton("butEqual").Click();
+            string calc = obj.GetTextBox("txtResult").BulkText;
+            Assert.AreEqual(res, calc);
+        }
+    }
+}
